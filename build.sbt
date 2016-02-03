586d84a40331b221147b06846f751869b75b4c06
Rebecca Sharp <bsharp@cottonwood.cs.arizona.edu>
2016-02-03 12:54:33 -0700

    added processors to build.sbt

--- build.sbt
+++ build.sbt
@@ -4,4 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
+libraryDependencies ++= Seq(
+  "org.clulab" %% "processors" % "5.8.0",
+  "org.clulab" %% "processors" % "5.8.0" classifier "models"
+)
+
+
     
--- src/main/scala/qa/Voter.scala
+++ src/main/scala/qa/Voter.scala
@@ -1,8 +1,20 @@
 package qa
 
+import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
+
 /**
   * Created by bsharp on 2/3/16.
   */
 class Voter {
 
+}
+
+object Voter {
+
+  def main(args:Array[String]): Unit = {
+    val processor = new FastNLPProcessor(withDiscourse = false)
+
+    
+
+  }
 }
