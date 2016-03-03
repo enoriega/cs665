375b6df20c8e90133cd7131d2f4332ab8673629e
Rebecca Sharp <bsharp@cottonwood.cs.arizona.edu>
2016-03-03 15:05:33 -0700

    in BUClassify added splitting into folds for train/dev/test, added some printlns and two simple length-based features

--- src/main/scala/qa/qtype/BottomUpClassify.scala
+++ src/main/scala/qa/qtype/BottomUpClassify.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import com.typesafe.config.ConfigFactory
 import edu.arizona.sista.learning.{LogisticRegressionClassifier, RVFDatum, RVFDataset}
+import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
 import edu.arizona.sista.struct.Counter
 import qa.input.{Question, InputReader}
 import qa.voting.Voter._
@@ -15,10 +16,24 @@ import qa.voting.Voter._
 
 object BottomUpClassify extends App {
 
+  lazy val processor = new FastNLPProcessor()
+
   // Opens a file, reads the questions and divides them into train, dev, and test partitions
-  def generateFoldsFromFile(fn:String):(Seq[Question], Seq[Question]) = {
+  def generateFoldsFromFile(fn:String):(Seq[Question], Seq[Question], Seq[Question]) = {
     val questions = new InputReader(new File(fn)).qTypeQuestions
-    ???
+    val numQuestions = questions.length
+    // Grab the first 60% questions for train
+    val (train, other) = questions.partition(questions.indexOf(_) <= (0.6*numQuestions))
+    val numOther = other.length
+    // Divide the rest into dev and test
+    val (dev, test) = other.partition(other.indexOf(_) <= (0.5*numOther))
+
+    println (s"After partitioning the $numQuestions questions...")
+    println (s"Train Questions: ${train.length}")
+    println (s"Dev Questions: ${dev.length}")
+    println (s"Test Questions: ${test.length}")
+
+    (train, dev, test)
   }
 
   // Make a Dataset for training out of the training questions
@@ -44,9 +59,23 @@ object BottomUpClassify extends App {
 
   // Extract a set of features from the question
   def mkFeatures(q:Question):Counter[String] = {
+    val qDoc = processor.annotate(q.question)
+    val aDocs = q.choices.map(a => processor.annotate(a.text))
     val fc = new Counter[String]
     // todo Turn on/Off Features?
     // TODO Make Features!
+    // Length Features:
+    val numSentsinQuestion = qDoc.sentences.length
+    fc.setCount("numSentsInQuestion", numSentsinQuestion)
+
+    val wordsInAns = for {
+      aDoc <- aDocs
+      s <- aDoc.sentences
+    } yield s.words.length
+    val avgWordsInAns:Double = wordsInAns.sum.toDouble / aDocs.size.toDouble
+    fc.setCount("avgWordsInEachAnswer", avgWordsInAns)
+
+    // TODO Unigram/Bigram features?
 
     fc
   }
@@ -73,7 +102,7 @@ object BottomUpClassify extends App {
     else ConfigFactory.parseFile(new File(args(0))).resolve()
 
   // 1. Load the training dataset
-  val (trainQuestions, testQuestions) = generateFoldsFromFile(config.getString("buc.questions"))
+  val (trainQuestions, devQuestions, testQuestions) = generateFoldsFromFile(config.getString("buc.questions"))
   val trainDataset = mkRVFDataset(trainQuestions)
 
   // 3. Train
@@ -81,7 +110,9 @@ object BottomUpClassify extends App {
   classifier.train(trainDataset)
 
   // 4. Predict
-  val testDatums = testQuestions.map(q => mkDatumFromQuestion(q))
+  val useDev = config.getBoolean("buc.useDev")
+  if (useDev) println ("Testing on DEV") else ("Testing on **TEST**")
+  val testDatums = if (useDev) devQuestions.map(q => mkDatumFromQuestion(q)) else testQuestions.map(q => mkDatumFromQuestion(q))
   val predictedLabels = testDatums.map(td => classifier.classOf(td))
 
   // 5. Evaluate
